066d0e343d188cad6f3953abedc43f78aecfc7c4
Ssssskkkkrrrraaaa <110767617+Ssssskkkkrrrraaaa@users.noreply.github.com>
2022-08-29 22:51:46 +0500

    Update AuraTrade_miniGame-HTML5-Shipping.UE4.js

--- AuraTrade_miniGame-HTML5-Shipping.UE4.js
+++ AuraTrade_miniGame-HTML5-Shipping.UE4.js
@@ -1145,7 +1145,7 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MAIN JS
-		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('UE4Game-HTML5-Shipping.js'), 'blob').then(function(data) {
+		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/Ssssskkkkrrrraaaa/-3/fd92bc1800906bcdd4641560f679477ec575b4a6/UE4Game-HTML5-Shipping.js'), 'blob').then(function(data) {
 				Module['mainScriptUrlOrBlob'] = data;
 				return addScriptToDom(data).then(function() {
 					addRunDependency('wait-for-compiled-code');
